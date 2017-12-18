5015f76e31b18f936ba8a25a099749eb644f02da
nstat <32303714+nstat@users.noreply.github.com>
2017-12-18 19:57:15 +0300

    Add files via upload

---
+++ huff.h
@@ -0,0 +1,29 @@
+int huff_compress(unsigned char * str, int N, unsigned char * strnew, table * arr) {
+    unsigned char buf = 0;
+    int cntbuf = 0;
+    int k, i, j;
+    int curnum;
+    int y = 0;
+    for (i = 0; i < N; ++i) {
+        unsigned char c = str[i];
+        j = 0;
+        while (arr[j].val != c)
+            ++j;
+        curnum = j;
+        for (k = 0; k < arr[curnum].cnt; ++k) {
+            if (cntbuf == 8) {
+                cntbuf = 0;
+                strnew[y++] = buf;
+                buf == 0;
+            }
+            buf = (buf << 1) | arr[curnum].str[k];
+            cntbuf++;
+        }
+    }
+    while (cntbuf != 8) {
+        buf = buf << 1;
+        cntbuf++;
+    }
+    strnew[y] = buf;
+    return y + 1;
+}
